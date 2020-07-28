dba6b21891ac7da385901cccb23f914bffa58101
Nicci Moyer <niccihendrix@yahoo.com>
2020-07-28 17:27:20 -0500

    question and answers showing

--- script.js
+++ script.js
@@ -1,214 +1,96 @@
-const question = document.getElementById('quizQuestions');
-const answers = Array.from(document.getElementsByClassName(''));
-
-let currentQuestion = {}
-
-
-function buildQuiz() {
-    const output = []
-    const answers = []
-
 
 
 let quizQuestions = [
-        {
-            question: "What does HTML stand for?",
-            answers: {
-                a: "Hyper Text Markup Language",
-                b: "Hairy Teachers Make Lasagna",
-                c: "Hyperlink Textual Markup Language",
-                d: "Hyper Text Markdown Language"
-            },
-            correctAnswer: "a",
-        },
-        {
-            question: "Which character is used to indicate an end tag?",
-            answers: {
-                a: ".",
-                b: "#",
-                c: "/",
-                d: "~"
-            },
-    correctAnswer: "c" ,
-        },
     {
-            question: "In what order do you designate border sizes?",
-            answers: {
-                a: "Top, right, bottom, left",
-                b: "Top, bottom, left, right",
-                c: "Left, right, top, bottom"
-            },
-    correctAnswer: "a"
-        },
-        {
-            question: "How would you select an element with the class name 'example'?", 
-    answers: {
-                a: "example",
-                b: "for (example)",
-                c: "#example",
-                d: ".example"
-            },
-            correctAnswer: "d",
-        },
-        {
-            question: "What will the following return: Boolean(5 > 10)",
-            answers: {
-                a: "True",
-                b: "False"
-            },
-    correctAnswer: "b",
-        },
-        {
-            question: "The bootstrap grid system is based on how many columns?",
-            answers: {
-                a: "6",
-                b: "1000",
-                c: "12",
-                d: "10"
-            },
-            correctAnswer: "c",
-        },
+        question: "What does HTML stand for?",
+         answers: [
+            "Hyper Text Markup Language",
+            "Hairy Teachers Make Lasagna",
+            "Hyperlink Textual Markup Language",
+            "Hyper Text Markdown Language"
+         ],
+        correctAnswer: "0",
+    },
     {
-            question: "Which plugin is used to cycle through elements, one after another, like a slideshow?",
-            answers: {
-                a: "Carousel",
-                b: "Fluid container",
-                c: "Merry-go-round",
-                d: "Cycle"
-            },
-            correctAnswer: "a"
+        question: "Which character is used to indicate an end tag?",
+         answers: [
+            ".",
+            "#",
+            "/",
+            "~"
+         ],
+correctAnswer: "c" ,
+    },
+{
+        question: "In what order do you designate border sizes?",
+         answers: {
+            a: "Top, right, bottom, left",
+            b: "Top, bottom, left, right",
+            c: "Left, right, top, bottom"
         },
-        {
-            question: "Javascript is case-sensitive.",
-            answers: {
-                a: "True",
-                b: "False"
-            },
-            correctAnswer: "a",
+correctAnswer: "a"
+    },
+    {
+        question: "How would you select an element with the class name 'example'?", 
+         answers: {
+            a: "example",
+            b: "for (example)",
+            c: "#example",
+            d: ".example"
         },
-        {
-            question: "How does a for loop start?",
-            answers: {
-                a: "let i = 0",
-                b: "for (i = 0; i > 5; i++)",
-                c: "if (condition) {} else {}",
-                d: "for (i > x;) let i = 0"
-            },
-            correctAnswer: "b",
+        correctAnswer: "d",
+    },
+    {
+        question: "The bootstrap grid system is based on how many columns?",
+         answers: {
+            a: "6",
+            b: "1000",
+            c: "12",
+            d: "10"
         },
-        {
-            question: "Javascript and Java are the same thing.",
-            answers: {
-                a: "True",
-                b: "False"
-            },
-    correctAnswer: "b",
+        correctAnswer: "c",
+    },
+{
+        question: "Which plugin is used to cycle through elements, one after another, like a slideshow?",
+         answers: {
+            a: "Carousel",
+            b: "Fluid container",
+            c: "Merry-go-round",
+            d: "Cycle"
         },
-     
-    ];
-    // quizQuestions.forEach((currentQuestion, questionNumber), 
+        correctAnswer: "a"
+    },
+    {
+        question: "How does a for loop start?",
+         answers: {
+            a: "let i = 0",
+            b: "for (i = 0; i > 5; i++)",
+            c: "if (condition) {} else {}",
+            d: "for (i > x;) let i = 0"
+        },
+        correctAnswer: "b",
+    },
+ 
+ 
+];
+
+let questionNameH3 = document.querySelector("#quizQuestions")
+
+//make a variable instead of 1 of something like current question
+questionNameH3.textContent = quizQuestions[currentQuestion].question
+
+
+let firstAnswerChoiceLi = document.querySelector("#answerChoice1")
+let secondAnswerChoiceLi = document.querySelector("#answerChoice2")
+let thirdAnswerChoiceLi = document.querySelector("#answerChoice3")
+let fourthAnswerChoiceLi = document.querySelector("#answerChoice4")
 
 
 
-    // for (letter in currentQuestion.answers) {
-    //     answers.push(
-    //         <label>
-    //             <input type="radio" name="question${questionNumber}" value="$letter">
-    //                 ${letter} :
-    //                     ${currentQuestion.answers[letter]}
-    //                 </label>
-    //             )
-    //         }
-    //         {output.push(
-    //                 <div class="question"> ${currentQuestion.question} </div>
-    //                 <div class="answers"> ${answers.join('')} </div>
-    //         )}, 
-    //     }
-    // )
-quizContainer.innerHTML = output.join('');
+firstAnswerChoiceLi.textContent = quizQuestions[1].answers[0]
+secondAnswerChoiceLi.textContent = quizQuestions[1].answers[1]
+thirdAnswerChoiceLi.textContent = quizQuestions[1].answers[2]
+fourthAnswerChoiceLi.textContent = quizQuestions[1].answers[3]
 
 
 
-//changes the color of the button when clicked
-
-    var themeSwitcher = document.querySelector("#theme-switcher");
-    var container = document.querySelector(".answers");
-    var mode = "dark";
-
-    themeSwitcher.addEventListener("click", function() {
-     if (mode === "dark") {
-                    mode = "light";
-        answers.setAttribute("class", "light");
-     }
-     else {
-                    mode = "dark";
-        answers.setAttribute("class", "dark");
-     }
-    });
-}
-
-
-
-
-
-
-{/* //insert timer alteration code here//
-function score() {
-    const answerBank = quizGroup.querySelectorAll('.answers');
-    let correctAnswer = 0;
-
-    quizQuestions.forEach(currentQuestion, questionNumber) => {
-
-        const answerBank = answerBank[questionNumbe];
-        const selector = 'input[name=question${questionNumber}]: checked';
-        const userAnswer = (answerBank. querySelector(selector) || {}).value;
- */
-                    // if (userAnswer === currentQuestion.correctAnswer) {
-
-                    //                         answerBank[questionNumber].style.color = 'green';
-                    // }
-                    // else {
-                    //                         answerBank[questionNumber].style.color = 'red';
-                    // })
-                    // resultsContainer.innerHTML = `${correctAnswer} out of ${quizQuestions.length}`;
-
-                };
-
-                
-                var quizTime = 120
-                var timer = document.getElementsByClassName(".timer");
-                var answers = document.getElementById("");
-                
-
-                function quizTimer() {
-                    append(timer);
-                  
-
-                        var timeInterva = setInterval(function() {
-                          timerEl.textContent = quizTime + " seconds remaining";
-                         quizTime--;
-                                         
-                
-                  
-                    }, 1000);
-                  }
-
-
-
-
-
-
-    {/* // Compare answers
-    if ((answer === true && questions[i].a === "t") ||
-        (answer === false && questions[i].a === "f")) {
-                                // Increase score
-                                score++;
-        alert("Correct!");
-    }
-    else {
-                                alert("Wrong!");
-    }
-}
-
-// Show total at end
-alert("You got " + score + "/" + questions.length); */ } 
